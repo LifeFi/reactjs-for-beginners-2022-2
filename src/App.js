174682f6accea58adaaf8f1481b4fb9c19559e14
topcircle <topcircle@gmail.com>
2022-12-07 17:57:37 +0900

    6.4 cleanup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import Button from "./Button";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useInsertionEffect } from "react";
 import styles from "./App.module.css";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  //console.log("i run all the time");
+function Hello() {
+  /*
+  function byeFn() {
+    console.log("Bye :)");
+  }
 
-  useEffect(() => {
-    console.log("Call the APIs...");
+  function hiFn() {
+    console.log("created!");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+*/
+  /*
+useEffect(function() {
+    console.log("created");
+    return function() {
+      console.log("destroyed");
+    };
   }, []);
+*/
   useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword or counter' changes");
-  }, [keyword, counter]);
+    console.log("created");
+    return () => console.log("destroyed");
+  }, []);
+
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h2>{counter}</h2>
-      <button onClick={onClick}>Click me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
