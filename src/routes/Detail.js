8f06c48a759a2dfce2b68d317f7ca3044896fb4e
topcircle <topcircle@gmail.com>
2022-12-08 10:45:18 +0900

    7.6

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState("");
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json.data);
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div>
+      <span>
+        <Link to="/">Back to Home</Link>
+      </span>
+      <Movie
+        key={movie.id}
+        id={movie.id}
+        coverImg={movie.medium_cover_image}
+        title={movie.title}
+        summary={movie.description_full}
+        genres={movie.genres}
+      />
+    </div>
+  );
 }
 export default Detail;
